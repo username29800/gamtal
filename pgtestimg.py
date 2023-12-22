8bcbd900e0bde1c3559cd4b2a8e6422c505a65f9
username29800 <piapia78490@gmail.com>
2023-12-22 18:21:16 +0900

    update dir structure

--- pgtestimg.py
+++ pgtestimg.py
@@ -1,17 +1,25 @@
 import pygame as pg
 pg.init()
-sti=pg.image.load("back.png")
+sti=pg.image.load("./asset/bg/back.png")
 screen=pg.display.set_mode((sti.get_rect().size))
 clock=pg.time.Clock()
 running=True
+r1=pg.Rect(50,50,100,100)
+r2=pg.Rect(10,10,100,70)
+pg.mouse.set_visible(False)
+tr=0
 while running:
   for event in pg.event.get():
     if event.type==pg.QUIT:
       running=False
   clock.tick(1024)
   screen.blit(sti,(0,0))
-  r1=pg.Rect(50,50,100,100)
-  r2=pg.Rect(10,10,100,70)
-  r1.move_ip(pg.mouse.get_pos()[0]-r1.left-r1.width/2,pg.mouse.get_pos()[1]-r1.top-r1.height/2)
+  r1.centerx,r1.centery=pg.mouse.get_pos()[0],pg.mouse.get_pos()[1]
+  if tr<7:
+    tr+=1
+  else:
+    r2.y+=1
+    tr=0
   pg.draw.rect(screen,"white",r1)
+  pg.draw.rect(screen,"blue",r2)
   pg.display.flip()
