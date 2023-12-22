d18dd6a55934886a5c8a671269300374e55e08b3
username29800 <piapia78490@gmail.com>
2023-12-22 19:43:13 +0900

    I want more

--- pgtestimg.py
+++ pgtestimg.py
@@ -1,31 +1,44 @@
 import pygame as pg
 pg.init()
+#background image
 sti=pg.image.load("./asset/bg/back.png")
+#screen Surface init
 screen=pg.display.set_mode((sti.get_rect().size))
+#clock init
 clock=pg.time.Clock()
 running=True
-r1=pg.Rect(200,200,100,100)
-r2=pg.Rect(10,10,100,70)
+#sprite rect
+# r1=pg.Rect(200,200,100,100)
+# r2=pg.Rect(10,10,100,70)
 #start button
 stbtn=pg.Rect(65,605,980,106)
+#cursor
+pg.mouse.set_cursor(pg.cursors.diamond)
+#tick regulator init
 tr=0 
 while running:
   for event in pg.event.get():
     if event.type==pg.QUIT:
       running=False
   clock.tick(1024)
+  screen.blit(sti,(0,0))
+  # collision detection
+  '''
   if r1.colliderect(r2):
     running=False
-  screen.blit(sti,(0,0))
   r1.centerx,r1.centery=pg.mouse.get_pos()[0],pg.mouse.get_pos()[1]
+  '''
+  # tick regulator
+  '''
   if tr<7:
     tr+=1
   else:
     r2.y+=1
     tr=0
-  pg.draw.rect(screen,(0,0,0,50),stbtn)
-  pg.draw.rect(screen,"blue",r2)
-  pg.draw.rect(screen,"white",r1)
+  '''
+  #pg.draw.rect(screen,(0,0,0,50),stbtn)
+  #pg.draw.rect(screen,"blue",r2)
+  #pg.draw.rect(screen,"white",r1)
   # start the game
   if stbtn.collidepoint(pg.mouse.get_pos()) and pg.event.get(pg.MOUSEBUTTONDOWN):
     print("game start signal")
